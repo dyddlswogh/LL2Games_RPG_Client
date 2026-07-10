2b0854fcacb89fe4ad593a90fba5a61243f8b41c
gunoo22 <gunoo22@ksign.com>
2026-07-10 16:05:26 +0900

    feat: M_LOGGER 추가 1. __FILE__, __FUNCTION__, __LINE__ 기본적용 2. fmt 적용가능 (printf와 같은형태로 쓸수있음)

--- WinAPITest_Source/stbLogger.h
+++ WinAPITest_Source/stbLogger.h
@@ -5,6 +5,11 @@
 #include <mutex>
 #include "stbNetworkConfig.h"
 
+//LogFormat
+#include <cstdarg>
+#include <cstdio>
+#include <Windows.h>
+
 namespace stb
 {
     class Logger
@@ -50,7 +55,63 @@ namespace stb
             }
             OutputDebugStringA(msg.c_str());
         }
+        
+        //__FILE__이 너무 길게 나오는경우때문에 파일명 하나만 보기위해 만듦
+        //기존: C:\Users\user\source\repos\LL2Games\test\WinAPITest_Source\stbAnimation.cpp
+        //적용: stbAnimation.cpp
+        static const char* BaseFileName(const char* path)
+        {
+            if (path == nullptr)
+                return "";
 
+            const char* file = path;
+
+            for (const char* p = path; *p != '\0'; ++p)
+            {
+                if (*p == '\\' || *p == '/')
+                {
+                    file = p + 1;
+                }
+            }
+
+            return file;
+        }
+
+        static void LogFormat(const char* fmt, ...)
+        {
+#ifdef __DEV_LOG
+            if (fmt == nullptr)
+                return;
+
+            va_list args;
+            va_start(args, fmt);
+
+            va_list argsCopy;
+            va_copy(argsCopy, args);
+
+            int len = std::vsnprintf(nullptr, 0, fmt, argsCopy);
+            va_end(argsCopy);
+
+            if (len < 0)
+            {
+                va_end(args);
+                return;
+            }
+
+            std::string msg;
+            msg.resize(len + 1);
+
+            std::vsnprintf(msg.data(), msg.size(), fmt, args);
+            va_end(args);
+
+            msg.resize(len);
+
+            // 자동 줄바꿈 추가
+            msg += '\n';
+
+            Log(msg);
+#endif
+        }
      
 
         static void Close()
@@ -67,4 +128,9 @@ namespace stb
         ss << msg; \
         stb::Logger::Log(ss.str()); \
     } while(0)
+
+#define M_LOGGER(fmt, ...) \
+    do { \
+        stb::Logger::LogFormat("[%s %s(%d)]" fmt, stb::Logger::BaseFileName(__FILE__), __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+    } while (0)
 }
